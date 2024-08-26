071aa4998380a5d7441cb95f712dc8a0feca82d0
Renan Hugentobler <renanrhugentobler@gmail.com>
2024-08-25 21:35:48 -0300

    Botão voltar

--- src/app/create/create.page.ts
+++ src/app/create/create.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-create',
   templateUrl: './create.page.html',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatePage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
   }
 
+  
+  
+}
+function windowBack(){
+  window.history.back();
 }
