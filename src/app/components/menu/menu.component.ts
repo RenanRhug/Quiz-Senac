1c931fbd9e8e7fb82ffaeaa9a858880f51e895e0
guitomesz <42675646+Guitomesz@users.noreply.github.com>
2024-09-16 21:19:30 -0300

    Menu trocando cor do texto
    kk

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '../../service/login.service'; 
+import { AuthService } from '../../service/login.service';
+import { ThemeService } from '../../shared/services/theme.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
 })
-export class MenuComponent  implements OnInit {
+export class MenuComponent implements OnInit {
   user = localStorage.getItem('currentUserName');
   darkMode = false;
 
-  constructor(private router: Router, 
-    private authService: AuthService,) { }
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private themeService: ThemeService
+  ) {}
 
   logout(): void {
     if (this.user) {
@@ -24,8 +28,23 @@ export class MenuComponent  implements OnInit {
       this.authService.storeToken('');
     }
   }
-
+  SwitchTheme() {
+    this.themeService.mode = !this.themeService.mode;
+  }
+  SwitchMenuColors() {
+    // desafio do dia hein... faz essa gambiarra aqui em 2 linhas (sem ajuda)
+    const elements = document.getElementsByClassName('menuContent');
+    if (this.themeService.mode) {
+      for (let i = 0; i < elements.length; i++) {
+        const element = elements[i] as HTMLElement;
+        element.style.setProperty('color', 'black');
+      }
+    } else {
+      for (let i = 0; i < elements.length; i++) {
+        const element = elements[i] as HTMLElement;
+        element.style.setProperty('color', 'white');
+      }
+    }
+  }
   ngOnInit() {}
-
- 
 }
